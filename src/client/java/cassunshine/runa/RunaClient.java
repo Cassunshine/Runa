ab17b102387a61f4364e62188a15d4b9bce1ba5b
Cassunshine <cass.stark.sunshine@gmail.com>
2025-05-20 12:24:51 -0400

    Initial commit

---
+++ src/client/java/cassunshine/runa/RunaClient.java
@@ -0,0 +1,10 @@
+package cassunshine.runa;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RunaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
